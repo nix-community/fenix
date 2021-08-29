d996cc3dcf90377554c2ac20fe9718cdbd94fe99
zimbatm <zimbatm@zimbatm.com>
2021-08-29 16:22:44 +0200

    fmt

--- flake.nix
+++ flake.nix
@@ -16,13 +16,12 @@
       "i686-linux"
       "x86_64-darwin"
       "x86_64-linux"
-    ]
-      (system:
-        import ./. {
-          inherit system rust-analyzer-src;
-          pkgs = nixpkgs.legacyPackages.${system};
-          lib = nixpkgs.lib;
-        });
+    ] (system:
+      import ./. {
+        inherit system rust-analyzer-src;
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = nixpkgs.lib;
+      });
 
     overlay = import ./lib/overlay.nix (pkgs: packages.${pkgs.system});
   };
