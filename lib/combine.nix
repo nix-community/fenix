671bf18cd6d9dab49581ffebe49ebe07e9e7ca1a
figsoda <figsoda@pm.me>
2022-11-06 21:32:10 -0500

    fix sysroot resolution

--- lib/combine.nix
+++ lib/combine.nix
@@ -1,13 +1,27 @@
-{ lib, symlinkJoin }:
+{ lib, symlinkJoin, stdenv }:
 
 name: paths:
 
 symlinkJoin {
   inherit name paths;
   postBuild = ''
-    if [ -d $out/bin ]; then
-      cp --remove-destination $(realpath $out/bin/*) $out/bin
-    fi
+    for file in $(find $out/bin -xtype f -maxdepth 1); do
+      install -m755 $(realpath "$file") $out/bin
+
+      ${lib.optionalString stdenv.isLinux ''
+        if isELF "$file"; then
+          patchelf --set-rpath $out/lib "$file" || true
+        fi
+      ''}
+
+      ${lib.optionalString stdenv.isDarwin ''
+        install_name_tool -add_rpath $out/lib "$file" || true
+      ''}
+    done
+
+    for file in $(find $out/lib -name "librustc_driver-*" -maxdepth 1); do
+      install $(realpath "$file") $out/lib
+    done
   '';
   meta.platforms = lib.platforms.all;
 }
